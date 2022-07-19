16b8e72a3a636c51dfa21b69877dcc2a5edae1c6
Tavi <taviiordache03@gmail.com>
2022-07-19 11:08:30 +0300

    mf-app part2

--- remote/src/App.jsx
+++ remote/src/App.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Counter from "./Counter";
+
 import "./index.css";
+
+import Counter from "./Counter";
 
 const App = () => (
   <div className="container">
     <div>Name: remote</div>
-    <div>Framework: react</div>
+     <div>Framework: react</div>
     <div>Language: JavaScript</div>
-    <div>CSS: Empty CSS</div>
-
+    <div>CSS: Empty CSS</div> 
+    <Counter/>
   </div>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
--- remote/src/Counter.jsx
+++ remote/src/Counter.jsx
@@ -1,12 +1,30 @@
-import { createSignal } from "react";
+//import { createSignal } from 'react-signal';
+import { useState } from 'react'
+import React from 'react';
 
-export default () => {
-  const [count, setCount] = createSignal(0);
+// export default () => {
+//     const [count, setCount] = createSignal(0);
+  
+//     return (
+//       <div>
+//         <div>Count = {count()}</div>
+//         <button onClick={() => setCount(count() + 1)}>Add One</button>
+//       </div>
+//     );
+//   };
 
-  return (
-    <div class="bg-blue-900 text-white p-5">
-      <div>Count = {count()}</div>
-      <button onClick={() => setCount(count() + 1)}>Add One</button>
-    </div>
-  );
-};
+  class Counter extends React.Component {
+    render() {
+        // const [count, setCount] = useState(0);
+      return(
+        <counter >
+          <div>
+           {/* <div>Count = {count}</div>
+           <button onClick={() => setCount(count + 1)}>Add One</button> */}
+           Counter
+         </div> 
+        </counter>
+      );
+    }
+  }
+  export default Counter;
