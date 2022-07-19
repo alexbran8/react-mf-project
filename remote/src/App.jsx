5b6075eaa7597165fc81431bfba64802c396b922
Tavi <taviiordache03@gmail.com>
2022-07-19 11:03:21 +0300

    mf-app

---
+++ remote/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Counter from "./Counter";
+import "./index.css";
+
+const App = () => (
+  <div className="container">
+    <div>Name: remote</div>
+    <div>Framework: react</div>
+    <div>Language: JavaScript</div>
+    <div>CSS: Empty CSS</div>
+
+  </div>
+);
+ReactDOM.render(<App />, document.getElementById("app"));
