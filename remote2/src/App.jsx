02d02e6bd49c0e6f7f6b41613f26575741b8c7fd
roxana20-flr <savuroxanaflorina20@yahoo.com>
2022-08-30 18:56:28 +0300

    redux

--- remote2/src/App.jsx
+++ remote2/src/App.jsx
@@ -4,9 +4,10 @@ import ReactDOM from "react-dom";
 import Exemplu from "./Exemplu";
 import {Provider} from "react-redux";
 import PostsContainer from './components/PostsContainer'
-import store from "./redux/store";
+// import store from "./redux/store";
 
 import "./index.css";
+
 
 const App = () => (
   <div className="container">
--- remote2/src/redux/store.js
+++ remote2/src/redux/store.js
@@ -1,9 +1,31 @@
 import { createStore } from 'redux'
 import rootReducer from './reducer'
+import throttle from 'lodash.throttle';
+import { loadState, saveState } from './localStorage';
 
+// const store = createStore(
+//     rootReducer,
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// );
+
+// export default store;
+
+const persistedState = loadState();
 const store = createStore(
     rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    persistedState
 );
+// store.subscribe(() => {
+//   saveState({
+//     posts: store.getState().posts
+//   });
+// });
 
-export default store;
+store.subscribe(throttle(() => {
+    saveState({
+      posts: store.getState().posts
+    });
+  }, 1000));
+
+  export default store;
+
